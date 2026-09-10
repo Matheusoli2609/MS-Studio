26a4d13b306b078a13c24f77fc908b0205c93ba0
Matheus <matheusoli.2609@gmail.com>
2026-09-10 17:49:09 -0300

    chore: configura TypeScript no projeto

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./styles/reset.css";
-import "./styles/variables.css";
+import "./styles/tokens.css";
 import "./styles/globals.css";
 
-import App from "./App.jsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
